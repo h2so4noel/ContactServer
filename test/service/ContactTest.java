c0eb9ba835a832f3a856cdb363e42a029ecf277e
h2so4noel <h2so4noel@gmail.com>
2014-09-24 13:19:11 +0700

    add javadoc to the tests

--- test/service/ContactTest.java
+++ test/service/ContactTest.java
@@ -2,27 +2,24 @@ package service;
 
 import static org.junit.Assert.*;
 
-import java.net.URI;
-
 import javax.ws.rs.core.Response.Status;
 
 import jetty.JettyMain;
 
 import org.eclipse.jetty.client.HttpClient;
-import org.eclipse.jetty.client.HttpResponse;
 import org.eclipse.jetty.client.api.ContentResponse;
 import org.eclipse.jetty.client.api.Request;
 import org.eclipse.jetty.client.util.StringContentProvider;
 import org.eclipse.jetty.http.HttpMethod;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.glassfish.jersey.server.ServerProperties;
-import org.glassfish.jersey.server.Uri;
 import org.junit.*;
 
-import contact.entity.Contact;
-
+/**
+ * JUnit test to test the Contact server in various request.
+ * Send the request to the service by sending GET, PUT, POST and DELETE. Two times for each test.
+ * @author Pawin Suthipornopas 5510546123
+ *
+ */
 
 public class ContactTest {
 	
@@ -31,6 +28,11 @@ public class ContactTest {
 	String url;
 	HttpClient client = new HttpClient();
 	
+	/**
+	 * This method starts the server and setup Clients and URL to prepare for the test.
+	 * URL is local on port 8080.
+	 * @throws Exception in case any error happens.
+	 */
 	@Before
 	public void beforeTest() throws Exception{
 		//Start Jetty
@@ -44,6 +46,10 @@ public class ContactTest {
 		url = "http://localhost:8080/contacts/";
 	}
 	
+	/**
+	 * Shutdown the server after the test, and close the client.
+	 * @throws Exception
+	 */
 	@After
 	public void afterTest() throws Exception {
 		//Stop Jetty
@@ -53,6 +59,11 @@ public class ContactTest {
 		client.stop();
 	}
 
+	/**
+	 * First GET test method.
+	 * Simply request GET without any path or parameters.
+	 * @throws Exception
+	 */
 	@Test
 	public void testGet1() throws Exception {
 		ContentResponse r = client.GET(url);
@@ -60,6 +71,11 @@ public class ContactTest {
 		assertEquals(Status.OK.getStatusCode(), r.getStatus());
 	}
 	
+	/**
+	 * Second GET test method.
+	 * Request it along with 10000 ID path. Should return error.
+	 * @throws Exception
+	 */
 	@Test
 	public void testGet2() throws Exception {
 		ContentResponse r = client.GET(url+10000);
@@ -67,6 +83,12 @@ public class ContactTest {
 		assertEquals(204, r.getStatus());
 	}
 	
+	/**
+	 * First POST test method.
+	 * Create a new Contact using StringContentProvider and send in as XML Application.
+	 * Should return a CREATED response code.
+	 * @throws Exception
+	 */
 	@Test
 	public void testPost1() throws Exception {
 		StringContentProvider contact = new StringContentProvider("<contact id=\"9595\">" +
@@ -83,6 +105,12 @@ public class ContactTest {
 		r = req.send();
 	}
 	
+	/**
+	 * Second POST test method.
+	 * Send in a new Contact the same way as previous, but there is already id = 9999 in the database.
+	 * So this one should return a false response.
+	 * @throws Exception
+	 */
 	@Test
 	public void testPost2() throws Exception {
 		// id = 9999 already exist.
@@ -96,6 +124,12 @@ public class ContactTest {
 		assertEquals(409, r.getStatus());
 	}
 	
+	/**
+	 * First PUT test method
+	 * Try editing current id = 1234.
+	 * Should return OK since 1234 exists.
+	 * @throws Exception
+	 */
 	@Test
 	public void testPut1() throws Exception {
 		Request r = client.newRequest(url+1234);
@@ -110,7 +144,12 @@ public class ContactTest {
 		ContentResponse res = r.send();
 		assertEquals(Status.OK.getStatusCode(), res.getStatus());
 	}
-	
+
+	/**
+	 * Second PUT test method.
+	 * Same as previous method, but the id 1239 doesn't exist so this one should get error response.
+	 * @throws Exception
+	 */
 	@Test
 	public void testPut2() throws Exception {
 		Request r = client.newRequest(url+1239);
@@ -126,6 +165,12 @@ public class ContactTest {
 		assertEquals(400, res.getStatus());
 	}
 	
+	/**
+	 * First DELETE test method.
+	 * Add new contact and delete it out using the indicated id.
+	 * Should return OK to confirm the deletion.
+	 * @throws Exception
+	 */
 	@Test
 	public void testDelete1() throws Exception {
 		//Add contact to test delete
@@ -142,6 +187,12 @@ public class ContactTest {
 		assertEquals(Status.OK.getStatusCode(), r.getStatus());
 		}
 	
+	/**
+	 * Second DELETE test method.
+	 * Simply try to delete something without a correct path.
+	 * Should return 405 response.
+	 * @throws Exception
+	 */
 	@Test
 	public void testDelete2() throws Exception {
 		//Test here failed because of no path.
